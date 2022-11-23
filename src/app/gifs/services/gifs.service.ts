eaf36ab78a7775048179fd501ea6cbf7bd2ca7ad
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-23 16:52:53 -0300

    peticiones con el modulo http client

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  private apiKey: string = '';
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) {}
 
   buscarGifs(query: string = '') {
     query = query.trim().toLocaleLowerCase();
@@ -17,5 +21,11 @@ export class GifsService {
     }
     this._historial = this._historial.splice(0, 10);
     console.log(this._historial);
+
+    this.http
+      .get('http://api.giphy.com/v1/gifs/search?api_key=hnFMnWNKHf0XO2LzPayYHkI9tYpoRjHe&q=Naruto&limit=10')
+      .subscribe((res: any) => {
+        console.log(res.data);
+      });
   }
 }
