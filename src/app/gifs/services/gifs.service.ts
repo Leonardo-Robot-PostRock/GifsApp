8a375bd4ef46dce9409fdd559cd2f65fd95a1b89
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-23 21:55:03 -0300

    se agrega la variable resultados para mostrar en el dom también se agregan los tipos ts

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-  private apiKey: string = '';
   private _historial: string[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
@@ -18,14 +19,17 @@ export class GifsService {
     query = query.trim().toLocaleLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
     }
-    this._historial = this._historial.splice(0, 10);
     console.log(this._historial);
 
     this.http
-      .get('http://api.giphy.com/v1/gifs/search?api_key=hnFMnWNKHf0XO2LzPayYHkI9tYpoRjHe&q=Naruto&limit=10')
+      .get<SearchGifsResponse>(
+        `http://api.giphy.com/v1/gifs/search?api_key=hnFMnWNKHf0XO2LzPayYHkI9tYpoRjHe&q=${query}&limit=10`
+      )
       .subscribe((res: any) => {
         console.log(res.data);
+        this.resultados = res.data;
       });
   }
 }
